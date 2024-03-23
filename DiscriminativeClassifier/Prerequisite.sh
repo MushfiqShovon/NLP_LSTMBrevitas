f328cf5bb96a166b0b9e366e259aabb7e4b2e5ea
MushfiqShovon <mushfiq.r.shovon@gmail.com>
2024-03-23 17:18:18 +0000

    Adding Prerequisite file

---
+++ DiscriminativeClassifier/Prerequisite.sh
@@ -0,0 +1,12 @@
+echo "======================"
+echo "Torchtext Installation"
+echo "======================"
+pip install torchtext==0.6.0
+echo "======================"
+echo "download en_core_web_sm"
+echo "======================"
+python -m spacy download en_core_web_sm
+echo "======================"
+echo "Installing Brevitas"
+echo "======================"
+pip install brevitas
