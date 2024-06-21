0a608c907a8e88afca02116de8d78aa5dd1028c0
MushfiqShovon <mushfiq.r.shovon@gmail.com>
2024-06-21 20:53:36 +0000

    Update models and remove old training scripts

--- DiscriminativeClassifier/models.py
+++ DiscriminativeClassifier/models.py
@@ -102,8 +102,14 @@ class QLSTM_1bit(nn.Module):
             bidirectional=bidirectional, 
             weight_bit_width=bit_witdh,
             io_bit_width=bit_witdh,
+            gate_acc_bit_width=bit_witdh,
+            sigmoid_bit_width=bit_witdh,
+            tanh_bit_width=bit_witdh,
             weight_quant = SignedBinaryWeightPerTensorConst, 
             io_quant = SignedBinaryActPerTensorConst,
+            gate_acc_quant=SignedBinaryActPerTensorConst,
+            sigmoid_quant=SignedBinaryActPerTensorConst,
+            tanh_quant=SignedBinaryActPerTensorConst,
             return_quant_tensor=True,
             batch_first=True)
 
@@ -145,8 +151,14 @@ class QLSTM_2bit(nn.Module):
             bidirectional=bidirectional, 
             weight_bit_width=bit_witdh,
             io_bit_width=bit_witdh,
+            gate_acc_bit_width=bit_witdh,
+            sigmoid_bit_width=bit_witdh,
+            tanh_bit_width=bit_witdh,
             weight_quant = SignedTernaryWeightPerTensorConst, 
             io_quant = SignedTernaryActPerTensorConst,
+            gate_acc_quant=SignedTernaryActPerTensorConst,
+            sigmoid_quant=SignedTernaryActPerTensorConst,
+            tanh_quant=SignedTernaryActPerTensorConst,
             return_quant_tensor=True,
             batch_first=True)
 
@@ -166,6 +178,170 @@ class QLSTM_2bit(nn.Module):
         
         padded_output,(hidden_state,cell_state) = self.quantLSTM(padded_embedded)
 
+        dense_outputs=self.fc(hidden_state)
+
+        return dense_outputs
+
+
+class RNNNet(nn.Module):
+    
+    def __init__(self,vocab_size,embedding_dim,hidden_dim,output_dim,n_layers,bidirectional,dropout, bit_witdh):
+        
+        super(RNNNet,self).__init__()
+        
+        self.embedding = nn.Embedding(vocab_size,embedding_dim)
+
+        self.rnn = nn.RNN(embedding_dim, hidden_dim, num_layers=n_layers, bidirectional=bidirectional, dropout=dropout, batch_first=True)
+        
+        self.fc = nn.Linear(hidden_dim, output_dim, bias=False)
+        
+        self.sigmoid = nn.Sigmoid()
+        
+        
+    
+    def forward(self,text,text_lengths):
+        embedded = self.embedding(text)
+
+        
+        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, text_lengths.cpu(),batch_first=True)
+        #padded_embedded = nn.utils.rnn.pad_sequence(embedded, batch_first=True)
+        
+        packed_output,hidden_state = self.rnn(packed_embedded)
+
+        hidden = hidden_state[-1,:,:]
+        
+        dense_outputs=self.fc(hidden)
+
+        return dense_outputs
+
+
+class QRNN(nn.Module):
+    
+    def __init__(self,vocab_size,embedding_dim,hidden_dim,output_dim,n_layers,bidirectional,dropout, bit_witdh):
+        
+        super(QRNN,self).__init__()
+        
+        self.embedding = nn.Embedding(vocab_size,embedding_dim)
+
+        self.qrnn = qnn.QuantRNN(
+            input_size=embedding_dim, 
+            hidden_size=hidden_dim, 
+            num_layers=n_layers, 
+            bidirectional=bidirectional, 
+            weight_bit_width=bit_witdh,
+            io_bit_width=bit_witdh,
+            gate_acc_bit_width=bit_witdh,
+            sigmoid_bit_width=bit_witdh,
+            tanh_bit_width=bit_witdh,
+            weight_quant = Int8WeightPerTensorFixedPoint, 
+            io_quant = Int8ActPerTensorFixedPoint,
+            gate_acc_quant=Int8ActPerTensorFixedPoint,
+            sigmoid_quant=Uint8ActPerTensorFixedPoint,
+            tanh_quant=Int8ActPerTensorFixedPoint,
+            return_quant_tensor=True,
+            batch_first=True)
+        
+        self.fc = qnn.QuantLinear(hidden_dim, output_dim, bias=False, weight_quant = Int8WeightPerTensorFixedPoint, weight_bit_width=bit_witdh)
+        
+        self.sigmoid = nn.Sigmoid()
+        
+        
+    
+    def forward(self,text,text_lengths):
+        embedded = self.embedding(text)
+
+        padded_embedded = nn.utils.rnn.pad_sequence(embedded, batch_first=True)
+        
+        packed_output,hidden_state = self.qrnn(padded_embedded)
+        
+        dense_outputs=self.fc(hidden_state)
+
+        return dense_outputs
+
+
+class QRNN_2bit(nn.Module):
+    
+    def __init__(self,vocab_size,embedding_dim,hidden_dim,output_dim,n_layers,bidirectional,dropout, bit_witdh):
+        
+        super(QRNN_2bit,self).__init__()
+        
+        self.embedding = nn.Embedding(vocab_size,embedding_dim)
+
+        self.qrnn = qnn.QuantRNN(
+            input_size=embedding_dim, 
+            hidden_size=hidden_dim, 
+            num_layers=n_layers, 
+            bidirectional=bidirectional, 
+            weight_bit_width=bit_witdh,
+            io_bit_width=bit_witdh,
+            gate_acc_bit_width=bit_witdh,
+            sigmoid_bit_width=bit_witdh,
+            tanh_bit_width=bit_witdh,
+            weight_quant = SignedTernaryWeightPerTensorConst, 
+            io_quant = SignedTernaryActPerTensorConst,
+            gate_acc_quant=SignedTernaryActPerTensorConst,
+            sigmoid_quant=SignedTernaryActPerTensorConst,
+            tanh_quant=SignedTernaryActPerTensorConst,
+            return_quant_tensor=True,
+            batch_first=True)
+        
+        self.fc = qnn.QuantLinear(hidden_dim, output_dim, bias=False, weight_quant = SignedTernaryWeightPerTensorConst, weight_bit_width=bit_witdh)
+        
+        self.sigmoid = nn.Sigmoid()
+        
+        
+    
+    def forward(self,text,text_lengths):
+        embedded = self.embedding(text)
+
+        padded_embedded = nn.utils.rnn.pad_sequence(embedded, batch_first=True)
+        
+        packed_output,hidden_state = self.qrnn(padded_embedded)
+        
+        dense_outputs=self.fc(hidden_state)
+
+        return dense_outputs
+
+
+class QRNN_1bit(nn.Module):
+    
+    def __init__(self,vocab_size,embedding_dim,hidden_dim,output_dim,n_layers,bidirectional,dropout, bit_witdh):
+        
+        super(QRNN_1bit,self).__init__()
+        
+        self.embedding = nn.Embedding(vocab_size,embedding_dim)
+
+        self.qrnn = qnn.QuantRNN(
+            input_size=embedding_dim, 
+            hidden_size=hidden_dim, 
+            num_layers=n_layers, 
+            bidirectional=bidirectional, 
+            weight_bit_width=bit_witdh,
+            io_bit_width=bit_witdh,
+            gate_acc_bit_width=bit_witdh,
+            sigmoid_bit_width=bit_witdh,
+            tanh_bit_width=bit_witdh,
+            weight_quant = SignedBinaryWeightPerTensorConst, 
+            io_quant = SignedBinaryActPerTensorConst,
+            gate_acc_quant=SignedBinaryActPerTensorConst,
+            sigmoid_quant=SignedBinaryActPerTensorConst,
+            tanh_quant=SignedBinaryActPerTensorConst,
+            return_quant_tensor=True,
+            batch_first=True)
+        
+        self.fc = qnn.QuantLinear(hidden_dim, output_dim, bias=False, weight_quant = SignedBinaryWeightPerTensorConst, weight_bit_width=bit_witdh)
+        
+        self.sigmoid = nn.Sigmoid()
+        
+        
+    
+    def forward(self,text,text_lengths):
+        embedded = self.embedding(text)
+
+        padded_embedded = nn.utils.rnn.pad_sequence(embedded, batch_first=True)
+        
+        packed_output,hidden_state = self.qrnn(padded_embedded)
+        
         dense_outputs=self.fc(hidden_state)
 
         return dense_outputs
